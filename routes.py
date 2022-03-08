4051a72ce14e708f3dd223eff9f77c4bd1756da5
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2022-03-08 19:37:51 +0200

    add response models

--- routes.py
+++ routes.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Body, Request, Response, HTTPException, status
 from fastapi.encoders import jsonable_encoder
+from typing import List
 
 from models import Book, BookUpdate
 
 router = APIRouter()
 
-@router.post("/", response_description="Create a new book", status_code=status.HTTP_201_CREATED)
+@router.post("/", response_description="Create a new book", status_code=status.HTTP_201_CREATED, response_model=Book)
 def create_book(request: Request, book: Book = Body(...)):
     book = jsonable_encoder(book)
     new_book = request.app.database["books"].insert_one(book)
@@ -16,12 +17,12 @@ def create_book(request: Request, book: Book = Body(...)):
     return created_book
 
 
-@router.get("/", response_description="List all books")
+@router.get("/", response_description="List all books", response_model=List[Book])
 def list_books(request: Request):
     books = list(request.app.database["books"].find(limit=100))
     return books
 
-@router.get("/{id}", response_description="Get a single book by id")
+@router.get("/{id}", response_description="Get a single book by id", response_model=Book)
 def find_book(id: str, request: Request):
     if (book := request.app.database["books"].find_one({"_id": id})) is not None:
         return book
@@ -29,7 +30,7 @@ def find_book(id: str, request: Request):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {id} not found")
 
 
-@router.put("/{id}", response_description="Update a book")
+@router.put("/{id}", response_description="Update a book", response_model=Book)
 def update_book(id: str, request: Request, book: BookUpdate = Body(...)):
     book = {k: v for k, v in book.dict().items() if v is not None}
 
