b8a79270cc5cc22594d7febd328357fa4e3cd208
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2022-03-14 10:37:52 +0200

    Add _id to model example

--- models.py
+++ models.py
@@ -12,6 +12,7 @@ class Book(BaseModel):
         allow_population_by_field_name = True
         schema_extra = {
             "example": {
+                "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
                 "title": "Don Quixote",
                 "author": "Miguel de Cervantes",
                 "synopsis": "..."
